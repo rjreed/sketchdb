9ef85b65c37b1d8f2640188312a89d95d17cf3d2
rjreed <rockyjreed@gmail.com>
2022-09-27 07:12:26 +0000

    removed fs-extra dev dependency

--- tests/index.test.js
+++ tests/index.test.js
@@ -8,7 +8,6 @@ const fsp = require('fs').promises
 const path = require('path');
 
 /// vendor libs
-const fse = require('fs-extra');
 
 /// app/local libs
 const carDB = require('../index.js');
@@ -21,14 +20,20 @@ const fixtures_path = path.join(__dirname, '/fixtures/');
 const tables_path = path.join(fixtures_path, '/tables/');
 const static_path = path.resolve(fixtures_path, 'static/');
 
-const table_1 = "posts"
-const table_2 = "authors"
-const table_3 = "users"
+const table_1 = "posts";
+const table_2 = "authors";
+const table_3 = "users";
 
 const table_1_path = path.join(tables_path, table_1);
 const table_2_path = path.join(tables_path, table_2);
 const table_3_path = path.join(tables_path, table_3);
 
+
+const path_exists = async function(path) {
+  return fsp.access(path)
+    .then( true)
+    .catch(() => false);
+};
 
 /// Data fixtures 
 //// TODO move this out and rename/ reorganize
@@ -38,55 +43,57 @@ const data_1 = {
   "author": "Basia Neal",
   "date": "Nov 27, 2022"
 };
+
 const data_2 = {
   "title": "sociosqu ad litora",
   "author": "Steven Burks",
   "date": "Nov 8, 2021"
-}
+};
+
 const data_3 = {
   "title": "erat. Vivamus nisi.",
   "author": "Aline Love",
   "date": "Nov 12, 2022"
-}
+};
 
 const data_3_update_1 = {
   "date": "Nov 14, 2022"
-}
+};
 
 const data_3_2 = {
   "title": "erat. Vivamus nisi.",
   "author": "Aline Love",
   "date": "Nov 14, 2022",
-}
+};
 
 const data_3_update_2 = {
   "coauthor": "Ben Bates"
-}
+};
 
 const data_3_3 = {
   "title": "erat. Vivamus nisi.",
   "author": "Aline Love",
   "date": "Nov 14, 2022",
   "coauthor": "Ben Bates"
-}
+};
 
 const data_4 = {
   "name": "Aline Love",
   "email": "alinelove@gmail.com",
   "group": "1"
-}
+};
 
 const data_5 = {
   "name": "Ben Bates",
   "email": "ben.bates@gmail.com",
   "group": "2"
-}
+};
 
 const data_6 = {
   "name": "Basia Neal",
   "email": "basiag.neal@gmail.com",
   "group": "2"
-}
+};
 
 const data_7 = [data_4, data_5, data_6];
 
@@ -94,7 +101,8 @@ const data_7 = [data_4, data_5, data_6];
 /// Tests
 
 test(`Test environment variables are setup`, () => {
-  expect(process.env.NODE_ENV).toBe('test')
+  expect(process.env.NODE_ENV).toBe('test');
+  
   expect(carDB._store).toBe(fixtures_path);
 });
 
@@ -109,7 +117,7 @@ test(`carDB.list_tables returns an array of the table names`, async () => {
 });
 
 test(`carDB.create_table creates a table`, async () => {
-  await carDB.create_table(table_3)
+  await carDB.create_table(table_3);
 
   const stat = await fsp.stat(path.join(tables_path, table_3));
   const _tables = await carDB.list_tables();
@@ -120,13 +128,13 @@ test(`carDB.create_table creates a table`, async () => {
 });
 
 test(`carDB.create_table rejects if given a duplicate table name`, async () => {
-  const create_call = carDB.create_table(table_1)
+  const create_call = carDB.create_table(table_1);
 
-  expect(create_call).rejects.toThrow();;
+  expect(create_call).rejects.toThrow();
 });
 
 test(`carDB.insert inserts an object into new row`, async () => {
-  await carDB.insert(table_1, 2, data_2)
+  await carDB.insert(table_1, 2, data_2);
 
 
   const retrieved_row = await carDB.get_row(table_1, 2);
@@ -136,16 +144,16 @@ test(`carDB.insert inserts an object into new row`, async () => {
 
 
 test(`carDB.insert rejects if given a duplicate row id`, async () => {
-  const insert_call = carDB.insert(table_1, 1, data_1)
+  const insert_call = carDB.insert(table_1, 1, data_1);
 
-  expect(insert_call).rejects.toThrow();;
+  expect(insert_call).rejects.toThrow();
 });
 
 
 
 
 test(`carDB.update modifies a key value pair in a row`, async () => {
-  await carDB.update(table_1, 1, data_3_update_1)
+  await carDB.update(table_1, 1, data_3_update_1);
 
   const retrieved_row = await carDB.get_row(table_1, 1);
 
@@ -153,7 +161,7 @@ test(`carDB.update modifies a key value pair in a row`, async () => {
 });
 
 test(`carDB.update can add a new key-value pair`, async () => {
-  await carDB.update(table_1, 1, data_3_update_2)
+  await carDB.update(table_1, 1, data_3_update_2);
 
   const retrieved_row = await carDB.get_row(table_1, 1);
 
@@ -161,43 +169,42 @@ test(`carDB.update can add a new key-value pair`, async () => {
 });
 
 test(`carDB.get_row returns a row's data`, async () => {
-  const retrieved_row = await carDB.get_row(table_1, 2)
+  const retrieved_row = await carDB.get_row(table_1, 2);
 
   expect(retrieved_row).toEqual(data_2);
 });
 
 test(`carDB.get_all returns all of a table's data`, async () => {
-  const retrieved_row = await carDB.get_all(table_2)
+  const retrieved_row = await carDB.get_all(table_2);
 
   expect(retrieved_row).toEqual(data_7);
 });
 
 test(`carDB.get_all rejects with an error if given an incorrect table name`, async () => {
-  const retrieved_row = carDB.get_all('incorrect_table')
+  const retrieved_row = carDB.get_all('incorrect_table');
 
   expect(retrieved_row).rejects.toThrow();
 });
 
 test(`carDB.delete_row deletes a row directory/file`, async () => {
-  await carDB.delete_row(table_1, 3)
+  await carDB.delete_row(table_1, 3);
 
-  const exists = await fse.pathExists(path.resolve(table_1_path, '3/'))
+  const exists = await path_exists(path.resolve(table_1_path, '3/'));
 
   expect(exists).toBe(false);
 });
 
 test(`carDB.filter return rows containing a given key-value pair`, async () => {
-  const retrieved_row = await carDB.filter(table_2, "group", "2")
+  const retrieved_row = await carDB.filter(table_2, "group", "2");
 
   expect(retrieved_row).toEqual([data_5, data_6]);
 });
 
 
-
 test(`carDB.delete_table deletes a table's directories/files`, async () => {
-  await carDB.delete_table(table_1)
+  await carDB.delete_table(table_1);
 
-  const exists = await fse.pathExists(table_1_path)
+  const exists = await path_exists(table_1_path);
 
   expect(exists).toBe(false);
 });
@@ -208,14 +215,14 @@ test(`carDB.delete_table deletes a table's directories/files`, async () => {
 
 //// delete all the test fixtures
 afterAll(() => {
- 
+
   return fsp.rm(tables_path, { recursive: true, force: true });
 
 });
 
 //// copy the static fixtures over to the tables path
 afterAll(async () => {
-  
-  return fsp.cp(static_path, tables_path,  { recursive: true })
+
+  return fsp.cp(static_path, tables_path, { recursive: true })
 
 });
